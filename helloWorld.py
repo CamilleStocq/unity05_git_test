6d083fca881f297fdff02a609a35942ddf655a56
NadTr <trauflernadine@gmail.com>
2025-06-06 16:03:41 +0200

    create conflict

--- helloWorld.py
+++ helloWorld.py
@@ -1,9 +1,14 @@
 from random import randint
 
-print ("*-()-*")
+print ("*-(-----------)-*")
 
 print("Hello World !")
+print("Goodbye World !")
 
 print(randint(1,6))
 
-print("The end")
+print("The beginning")
+
+
+print  ("Le ciel s’est libéré de ses vapeurs torrides, \n Les jours se sont défaits des trop vives clartés, \n L’air s’est enfin rempli d’une tiédeur humide, \n Le calme est revenu, l’été s’en est allé.")
+
