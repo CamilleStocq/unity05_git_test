c6aa5b2d39c3c77b58ea23d368056e6760c83c7d
Geneviève Ernould <gen_ernould@hotmail.com>
2025-06-06 15:16:56 +0200

    bon j'ai aussi ajouté un fichier des_trucs.py en plus de un_teste.txt

---
+++ des_trucs.py
@@ -0,0 +1,12 @@
+print("Hello Nadine et Camille!")
+
+print("Cadavre exquis :")
+
+a = "senpai"
+b = "..."
+c = "..."
+
+print("On peut mettre un verbe en b et un autre mot en c (nom/complément d'objet direct ou attribut du sujet, sorry si vous aimez pas la grammaire)")
+
+print(f"{a} {b} {c}")
+
